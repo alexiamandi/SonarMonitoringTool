3cf634dc50b773504e88ed9b6861a43907a01393
alexiamandi <alexiamandi@gmail.com>
2016-03-29 16:02:39 +0300

    29/3 second commit

--- SonarMonitoringTool/static/hello.js
+++ SonarMonitoringTool/static/hello.js
@@ -1,40 +1,52 @@
-var myApp = angular.module('myApp',['ngRoute']);
+var myApp = angular.module('myApp', [ 'ngRoute' ]);
 
-myApp.config(function($routeProvider,$locationProvider){
-                    $routeProvider
-                    	.when('/project/:projectId',{
-		                    controller:'GetProjectCtrl',
-		                    templateUrl:'project.html'
-		                })
+myApp.config(function($routeProvider, $locationProvider) {
+	$routeProvider.when('/project/:projectId', {
+		controller : 'GetProjectCtrl',
+		templateUrl : 'project.html'
+	})
 
-	$locationProvider.html5Mode(true); 		 
-});              
-myApp.controller('Hello', ['$scope', '$http', function($scope, $http) {
-    $http.get('http://localhost:8080/projects').
-    success(function(data) {
-        $scope.projects = data;
-    });
-}]);
+	$locationProvider.html5Mode(true);
+});
+myApp.controller('Hello', [ '$scope', '$http', function($scope, $http) {
+	$http.get('http://localhost:8080/projects').success(function(data) {
+		$scope.projects = data;
+	});
+} ]);
 
-myApp.controller('URLCtrl',['$window','$scope', '$http', function($window, $scope, $http) {
- 
-  $scope.saveURL = function(URLInfo) {
-		if(URLInfo.fURL.substring(4, 5) == 's')
-			var URL = 'http://localhost:8080/newURL/'+URLInfo.fURL.substring(8)+'/true';
-		else
-			var URL = 'http://localhost:8080/newURL/'+URLInfo.fURL.substring(7)+'/false'
-		console.log(URL);
-		$http.get(URL).
-		success(function(data) {
-				$window.location.href = 'sonar-projects.html';
-    });
-  }
-}]);
+myApp.controller('URLCtrl', [
+		'$window',
+		'$scope',
+		'$http',
+		function($window, $scope, $http) {
+			$scope.loadingClass = "loading-hidden";
+			$scope.saveButtonSwitch = false;
+			$scope.saveURL = function(URLInfo) {
+				$scope.loadingClass = "loading-visible";
+				$scope.saveButtonSwitch = true;
+				if (URLInfo.fURL.substring(4, 5) == 's')
+					var URL = 'http://localhost:8080/newURL/'
+							+ URLInfo.fURL.substring(8) + '/true';
+				else
+					var URL = 'http://localhost:8080/newURL/'
+							+ URLInfo.fURL.substring(7) + '/false'
+				console.log(URL);
+				$http.get(URL).success(function(data) {
+					$window.location.href = 'sonar-projects.html';
+				});
+			}
+		} ]);
 
-myApp.controller('GetProjectCtrl', ['$location','$scope','$http', function($location,$scope,$http){
-		$http.get('http://localhost:8080/project/'+$location.search().projectId).
-    success(function(data) {
-        $scope.project = data;
-    });
-	console.log($location.search().projectId);
-}]);
+myApp.controller('GetProjectCtrl', [
+		'$location',
+		'$scope',
+		'$http',
+		function($location, $scope, $http) {
+			$http.get(
+					'http://localhost:8080/project/'
+							+ $location.search().projectId).success(
+					function(data) {
+						$scope.project = data;
+					});
+			console.log($location.search().projectId);
+		} ]);
